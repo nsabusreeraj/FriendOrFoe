ece8966fae58f5dc709547756ad5b0db7313c2bc
nsabusreeraj <nsabusreeraj@salesforce.com>
2016-12-19 14:41:48 +0000

    aaa

---
+++ FriendOrFoe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace FriendOrFoe
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+    }
+
+    public static class Kata
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            foreach (var str in names)
+            {
+                Console.WriteLine(str);
+            }
+            
+            var friends = names?.Where(name => name.Length == 4);
+            return friends;
+        }
+    }
+
+}
---
+++ FriendOrFoeTests/KataTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FriendOrFoe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CollectionAssert = NUnit.Framework.CollectionAssert;
+
+namespace FriendOrFoe.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void FriendOrFoeTest1()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            CollectionAssert.AreEqual(expected, Kata.FriendOrFoe(names));
+        }
+        [TestMethod()]
+        public void FriendOrFoeTest2()
+        {
+            string[] expected = { "", "Mark" };
+            string[] names = { "Ryan", "", "Mark", "Jimmy" };
+            CollectionAssert.AreNotEqual(expected, Kata.FriendOrFoe(names));
+        }
+        [TestMethod()]
+        public void FriendOrFoeTest3()
+        {
+            string[] expected = { "" };
+            string[] names = { "Ryan", "", "Mark", "Jimmy" };
+            CollectionAssert.AreNotEqual(expected, Kata.FriendOrFoe(names));
+        }
+        [TestMethod()]
+        public void FriendOrFoeTest4()
+        {
+            string[] expected = { "" };
+            string[] names = { "Ryan", "Mark", "Jimmy" };
+            CollectionAssert.AreNotEqual(expected, Kata.FriendOrFoe(names));
+        }
+        [TestMethod()]
+        public void FriendOrFoeTest5()
+        {
+            string[] expected = {};
+            string[] names = { "Ryan", "Mark", "Jimmy" };
+            CollectionAssert.AreNotEqual(expected, Kata.FriendOrFoe(names));
+        }
+        [TestMethod()]
+        public void FriendOrFoeTest6()
+        {
+            string[] expected = { "Ryan" };
+            string[] names = { };
+            CollectionAssert.AreNotEqual(expected, Kata.FriendOrFoe(names));
+        }
+    }
+}
